a032ec11c35657e33926a2cc8002606543f2cb7e
PavanGovardhanan <30715312+PavanGovardhanan@users.noreply.github.com>
2017-08-18 19:39:13 +0530

    Add files via upload

--- src/main/java/commonMethods/Config.java
+++ src/main/java/commonMethods/Config.java
@@ -17,7 +17,7 @@ public class Config {
 		String ie = Utils.getDataFromTestConfig("IEDriverPath");
 
 		if (browserName.equals("Chrome")) {
-			System.setProperty("webdriver.chrome.driver", "/home/ec2-user/.jenkins/workspace/Tookitaki/driver/chromedriver");
+			System.setProperty("webdriver.chrome.driver", "/usr/bin/google-chrome");
 			System.out.println("Browser launched...");
 			driver = new ChromeDriver();
 			//ATUReports.setWebDriver(driver);
