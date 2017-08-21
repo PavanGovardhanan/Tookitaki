b0b113a4c2a42ee8b0500c3266e652f3bdffacc0
PavanGovardhanan <30715312+PavanGovardhanan@users.noreply.github.com>
2017-08-21 16:54:45 +0530

    Add files via upload

--- src/main/java/commonMethods/TestNgXml.java
+++ src/main/java/commonMethods/TestNgXml.java
@@ -170,7 +170,7 @@ public class TestNgXml {
 	        	 }
 	         }
 	         
-	         Element listeners = doc.createElement("listeners");
+	        /* Element listeners = doc.createElement("listeners");
 	         rootElement.appendChild(listeners);
 	         
 	         String a[]={"atu.testng.reports.listeners.ATUReportsListener","atu.testng.reports.listeners.ConfigurationListener","atu.testng.reports.listeners.MethodListener"};
@@ -179,13 +179,13 @@ public class TestNgXml {
 	        	 Element listener = doc.createElement("listener");
 		         listeners.appendChild(listener);
 		         listener.setAttribute("class-name", a[i]);
-	         }
+	         }*/
 	        
 	         // write the content into xml file
 	         TransformerFactory transformerFactory =TransformerFactory.newInstance();
 	         Transformer transformer = transformerFactory.newTransformer();
 	         DOMSource source = new DOMSource(doc);
-	         StreamResult result =new StreamResult(new File("D:\\Tookitaki\\testng.xml"));
+	         StreamResult result =new StreamResult("D:\\Tookitaki\\testng.xml");
 	         transformer.transform(source, result);
 	         // Output to console for testing
 	         StreamResult consoleResult =
