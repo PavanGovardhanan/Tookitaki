4fb6ca20e742db8fe43b8705f9e7c4f01244a4ac
PavanGovardhanan <30715312+PavanGovardhanan@users.noreply.github.com>
2017-08-30 11:06:02 +0530

    Add files via upload

--- src/main/java/commonMethods/OR.java
+++ src/main/java/commonMethods/OR.java
@@ -56,7 +56,7 @@ public interface OR {
 	public static String description = "Description >//div[@id='page-wrapper']/div[4]/div[2]/div[2]/div/div[2]/div/form/span/span/div[3]/div/input";
 	public static String infoNextStep = "Next Step >//div[@id='page-wrapper']/div[4]/div[2]/div[2]/div/div[2]/div/form/span[2]/button";
 	public static String dependentVariable = "Dependent Variable >//div[@id='page-wrapper']/div[4]/div[2]/div[2]/div/div[2]/div/form/span/div/div/select";
-	public static String selectDependentVariable = "Select Dependent Variable >//option[@value='53']";
+	public static String selectDependentVariable = "Select Dependent Variable >//option[@value='54']";
 	public static String selectDependentVariable1 = "Select Dependent Variable >//option[@value='10']";
 	public static String modelNextStep = "Next Step >//div[@id='page-wrapper']/div[4]/div[2]/div[2]/div/div[2]/div/form/span[2]/button[2]";
 	public static String analyzerType = "Analyzer Type >//div[@id='page-wrapper']/div[4]/div[2]/div[2]/div/div[2]/div/form/span/div/div/select";
